c66c7fd6950bfe466373bee3f443791d1f058332
Ryan Mitchell <webmin@rmitchellmedia.com>
2017-09-08 13:54:37 -0600

    Disable Theme

--- library/disable-theme-plugin-editors.php
+++ library/disable-theme-plugin-editors.php
@@ -5,4 +5,10 @@
  * Date: 9/5/17
  * Time: 10:20 AM
  */
-define(‘DISALLOW_FILE_EDIT’, true);
+add_action('admin_init', 'my_remove_menu_elements', 102);
+
+function my_remove_menu_elements()
+{
+	remove_submenu_page( 'themes.php', 'theme-editor.php' );
+	remove_submenu_page( 'plugins.php','plugin-editor.php' );
+}
